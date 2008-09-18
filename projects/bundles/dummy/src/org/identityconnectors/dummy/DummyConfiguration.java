b9361c450c08961d206525d977c0afce4c07e161
george_hetrick <george_hetrick@sun.com>
2008-09-18 15:36:05 +0000

    added dummy connector
    git-svn-id: https://svn.forgerock.org/openicf/trunk@93 05b3e5af-d696-470f-a577-fd7599f74d3c

---
+++ projects/bundles/dummy/src/org/identityconnectors/dummy/DummyConfiguration.java
@@ -0,0 +1,443 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * U.S. Government Rights - Commercial software. Government users 
+ * are subject to the Sun Microsystems, Inc. standard license agreement
+ * and applicable provisions of the FAR and its supplements.
+ * 
+ * Use is subject to license terms.
+ * 
+ * This distribution may include materials developed by third parties.
+ * Sun, Sun Microsystems, the Sun logo, Java and Project Identity 
+ * Connectors are trademarks or registered trademarks of Sun 
+ * Microsystems, Inc. or its subsidiaries in the U.S. and other
+ * countries.
+ * 
+ * UNIX is a registered trademark in the U.S. and other countries,
+ * exclusively licensed through X/Open Company, Ltd. 
+ * 
+ * -----------
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved. 
+ * 
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License(CDDL) (the License).  You may not use this file
+ * except in  compliance with the License. 
+ * 
+ * You can obtain a copy of the License at
+ * http://identityconnectors.dev.java.net/CDDLv1.0.html
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ * 
+ * When distributing the Covered Code, include this CDDL Header Notice in each
+ * file and include the License file at identityconnectors/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the fields 
+ * enclosed by brackets [] replaced by your own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * -----------
+ */
+package org.identityconnectors.dummy;
+
+import java.io.File;
+import java.net.URI;
+import org.identityconnectors.framework.spi.AbstractConfiguration;
+
+public class DummyConfiguration extends AbstractConfiguration
+{
+
+    public DummyConfiguration()
+    {
+        _string = "String";
+        _integer = new Integer(1);
+        _long = new Long(2L);
+        _boolean = Boolean.FALSE;
+        _float = new Float(3F);
+        _double = new Double(4D);
+        _int = 5;
+        _char = 'x';
+        _character = Character.valueOf('C');
+        _doublePrim = 6D;
+        _floatPrim = 7F;
+        _booleanPrim = false;
+        _longPrim = 8L;
+        try
+        {
+            _uri = new URI("http:/localhost:8080/foo");
+            _file = new File("dummy.txt");
+        }
+        catch(Exception e) { }
+        _booleanArray = (new Boolean[] {
+            Boolean.TRUE, Boolean.FALSE
+        });
+        try
+        {
+            _uriArray = (new URI[] {
+                new URI("http:/localhost:8080/foo"), new URI("http:/localhost:8080/bar")
+            });
+            _fileArray = (new File[] {
+                new File("foo.txt"), new File("bar.txt")
+            });
+        }
+        catch(Exception e) { }
+    }
+
+    public void validate()
+    {
+    }
+
+    public String getString()
+    {
+        return _string;
+    }
+
+    public void setString(String _string)
+    {
+        this._string = _string;
+    }
+
+    public Integer getInteger()
+    {
+        return _integer;
+    }
+
+    public void setInteger(Integer _integer)
+    {
+        this._integer = _integer;
+    }
+
+    public Long getLong()
+    {
+        return _long;
+    }
+
+    public void setLong(Long _long)
+    {
+        this._long = _long;
+    }
+
+    public long getLongPrim()
+    {
+        return _longPrim;
+    }
+
+    public void setLong(long _longPrim)
+    {
+        this._longPrim = _longPrim;
+    }
+
+    public Boolean getBoolean()
+    {
+        return _boolean;
+    }
+
+    public void setBoolean(Boolean _boolean)
+    {
+        this._boolean = _boolean;
+    }
+
+    public Float getFloat()
+    {
+        return _float;
+    }
+
+    public void setFloat(Float _float)
+    {
+        this._float = _float;
+    }
+
+    public Double getDouble()
+    {
+        return _double;
+    }
+
+    public void setDouble(Double _double)
+    {
+        this._double = _double;
+    }
+
+    public URI getUri()
+    {
+        return _uri;
+    }
+
+    public void setUri(URI _uri)
+    {
+        this._uri = _uri;
+    }
+
+    public int getInt()
+    {
+        return _int;
+    }
+
+    public void setInt(int _int)
+    {
+        this._int = _int;
+    }
+
+    public char getChar()
+    {
+        return _char;
+    }
+
+    public void setChar(char _char)
+    {
+        this._char = _char;
+    }
+
+    public Character getCharacter()
+    {
+        return _character;
+    }
+
+    public void setCharacter(Character character)
+    {
+        _character = character;
+    }
+
+    public double getDoublePrim()
+    {
+        return _doublePrim;
+    }
+
+    public void setDoublePrim(double prim)
+    {
+        _doublePrim = prim;
+    }
+
+    public float getFloatPrim()
+    {
+        return _floatPrim;
+    }
+
+    public void setFloatPrim(float prim)
+    {
+        _floatPrim = prim;
+    }
+
+    public boolean isBooleanPrim()
+    {
+        return _booleanPrim;
+    }
+
+    public void setBooleanPrim(boolean prim)
+    {
+        _booleanPrim = prim;
+    }
+
+    public File getFile()
+    {
+        return _file;
+    }
+
+    public void setFile(File _file)
+    {
+        this._file = _file;
+    }
+
+    public char[] getCharArray()
+    {
+        return _charArray;
+    }
+
+    public void setCharArray(char array[])
+    {
+        _charArray = array;
+    }
+
+    public String[] getStringArray()
+    {
+        return _stringArray;
+    }
+
+    public void setStringArray(String _stringArray[])
+    {
+        this._stringArray = _stringArray;
+    }
+
+    public Integer[] getIntegerArray()
+    {
+        return _integerArray;
+    }
+
+    public void setIntegerArray(Integer _integerArray[])
+    {
+        this._integerArray = _integerArray;
+    }
+
+    public Long[] getLongArray()
+    {
+        return _longArray;
+    }
+
+    public void setLongArray(Long _longArray[])
+    {
+        this._longArray = _longArray;
+    }
+
+    public long[] getLongPrimArray()
+    {
+        return _longPrimArray;
+    }
+
+    public void setLongArray(long _longPrimArray[])
+    {
+        this._longPrimArray = _longPrimArray;
+    }
+
+    public Boolean[] getBooleanArray()
+    {
+        return _booleanArray;
+    }
+
+    public void setBooleanArray(Boolean _booleanArray[])
+    {
+        this._booleanArray = _booleanArray;
+    }
+
+    public Float[] getFloatArray()
+    {
+        return _floatArray;
+    }
+
+    public void setFloatArray(Float _floatArray[])
+    {
+        this._floatArray = _floatArray;
+    }
+
+    public Double[] getDoubleArray()
+    {
+        return _doubleArray;
+    }
+
+    public void setDoubleArray(Double _doubleArray[])
+    {
+        this._doubleArray = _doubleArray;
+    }
+
+    public URI[] getUriArray()
+    {
+        return _uriArray;
+    }
+
+    public void setUriArray(URI _uriArray[])
+    {
+        this._uriArray = _uriArray;
+    }
+
+    public int[] getIntArray()
+    {
+        return _intArray;
+    }
+
+    public void setIntArray(int _intArray[])
+    {
+        this._intArray = _intArray;
+    }
+
+    public Character[] getCharacterArray()
+    {
+        return _characterArray;
+    }
+
+    public void setCharacterArray(Character characterArray[])
+    {
+        _characterArray = characterArray;
+    }
+
+    public double[] getDoublePrimArray()
+    {
+        return _doublePrimArray;
+    }
+
+    public void setDoublePrimArray(double primArray[])
+    {
+        _doublePrimArray = primArray;
+    }
+
+    public float[] getFloatPrimArray()
+    {
+        return _floatPrimArray;
+    }
+
+    public void setFloatPrimArray(float primArray[])
+    {
+        _floatPrimArray = primArray;
+    }
+
+    public boolean[] getBooleanPrimArray()
+    {
+        return _booleanPrimArray;
+    }
+
+    public void setBooleanPrimArray(boolean primArray[])
+    {
+        _booleanPrimArray = primArray;
+    }
+
+    public File[] getFileArray()
+    {
+        return _fileArray;
+    }
+
+    public void setFileArray(File _fileArray[])
+    {
+        this._fileArray = _fileArray;
+    }
+
+    private char _charArray[] = {
+        'c', 'h', 'a', 'r', 'A', 'r', 'r', 'a', 'y'
+    };
+    private String _string;
+    private Integer _integer;
+    private Long _long;
+    private Boolean _boolean;
+    private Float _float;
+    private Double _double;
+    private URI _uri;
+    private int _int;
+    private char _char;
+    private Character _character;
+    private double _doublePrim;
+    private float _floatPrim;
+    private boolean _booleanPrim;
+    private long _longPrim;
+    private File _file;
+    private String _stringArray[] = {
+        "String", "array"
+    };
+    private Integer _integerArray[] = {
+        Integer.valueOf(1), Integer.valueOf(2)
+    };
+    private Long _longArray[] = {
+        Long.valueOf(3L), Long.valueOf(4L)
+    };
+    private Boolean _booleanArray[];
+    private Float _floatArray[] = {
+        Float.valueOf(5F), Float.valueOf(6F)
+    };
+    private Double _doubleArray[] = {
+        Double.valueOf(7D), Double.valueOf(8D)
+    };
+    private URI _uriArray[];
+    private int _intArray[] = {
+        15, 16
+    };
+    private Character _characterArray[] = {
+        Character.valueOf('C'), Character.valueOf('h')
+    };
+    private double _doublePrimArray[] = {
+        9D, 10D
+    };
+    private float _floatPrimArray[] = {
+        11F, 12F
+    };
+    private boolean _booleanPrimArray[] = {
+        true, false
+    };
+    private long _longPrimArray[] = {
+        13L, 14L
+    };
+    private File _fileArray[];
+}
---
+++ projects/bundles/dummy/src/org/identityconnectors/dummy/DummyConnector.java
@@ -0,0 +1,290 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * U.S. Government Rights - Commercial software. Government users 
+ * are subject to the Sun Microsystems, Inc. standard license agreement
+ * and applicable provisions of the FAR and its supplements.
+ * 
+ * Use is subject to license terms.
+ * 
+ * This distribution may include materials developed by third parties.
+ * Sun, Sun Microsystems, the Sun logo, Java and Project Identity 
+ * Connectors are trademarks or registered trademarks of Sun 
+ * Microsystems, Inc. or its subsidiaries in the U.S. and other
+ * countries.
+ * 
+ * UNIX is a registered trademark in the U.S. and other countries,
+ * exclusively licensed through X/Open Company, Ltd. 
+ * 
+ * -----------
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved. 
+ * 
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License(CDDL) (the License).  You may not use this file
+ * except in  compliance with the License. 
+ * 
+ * You can obtain a copy of the License at
+ * http://identityconnectors.dev.java.net/CDDLv1.0.html
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.  
+ * 
+ * When distributing the Covered Code, include this CDDL Header Notice in each
+ * file and include the License file at identityconnectors/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the fields 
+ * enclosed by brackets [] replaced by your own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * -----------
+ */
+package org.identityconnectors.dummy;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import org.identityconnectors.framework.common.FrameworkUtil;
+import org.identityconnectors.framework.common.exceptions.UnknownUidException;
+import org.identityconnectors.framework.common.objects.*;
+import org.identityconnectors.framework.common.objects.filter.FilterTranslator;
+import org.identityconnectors.framework.spi.Configuration;
+import org.identityconnectors.framework.spi.Connector;
+import org.identityconnectors.framework.spi.ConnectorClass;
+import org.identityconnectors.framework.spi.operations.*;
+
+@ConnectorClass(displayNameKey="DummyConnector",configurationClass=DummyConfiguration.class)
+public class DummyConnector
+    implements Connector, CreateOp, DeleteOp, SearchOp, UpdateOp, SchemaOp
+{
+
+    public DummyConnector()
+    {
+    }
+
+    public void dispose()
+    {
+    }
+
+    public Configuration getConfiguration()
+    {
+        return _configuration;
+    }
+
+    public void init(Configuration cfg)
+    {
+        _configuration = (DummyConfiguration)cfg;
+        _connection = new DummyConnection();
+        _schema = schema();
+    }
+
+    public Uid create(ObjectClass objClass, Set attrs, OperationOptions options)
+    {
+        validateAttributes(objClass, attrs, true);
+        Uid uid = generateUid();
+        _map.put(uid, attrs);
+        return uid;
+    }
+
+    private void validateAttributes(ObjectClass oclass, Set attrs, boolean checkRequired)
+    {
+        ObjectClassInfo oci = _schema.findObjectClassInfo(oclass.getObjectClassValue());
+        Map attrMap = new HashMap(AttributeUtil.toMap(attrs));
+        for(Iterator i$ = oci.getAttributeInfo().iterator(); i$.hasNext();)
+        {
+            AttributeInfo attributeInfo = (AttributeInfo)i$.next();
+            if(checkRequired && attributeInfo.isRequired() && !attrMap.containsKey(attributeInfo.getName()))
+                throw new IllegalArgumentException((new StringBuilder()).append("Required attribute ").append(attributeInfo.getName()).append(" is missing").toString());
+            if(!(attributeInfo.isCreateable() || attributeInfo.isUpdateable()) && attrMap.containsKey(attributeInfo.getName()))
+                throw new IllegalArgumentException((new StringBuilder()).append("Non-writeable attribute ").append(attributeInfo.getName()).append(" is present").toString());
+        }
+
+        Map ociMap = AttributeInfoUtil.toMap(oci.getAttributeInfo());
+        for(Iterator i$ = attrs.iterator(); i$.hasNext();)
+        {
+            Attribute attribute = (Attribute)i$.next();
+            if(!ociMap.containsKey(attribute.getName()))
+                throw new IllegalArgumentException((new StringBuilder()).append("Unknown attribute ").append(attribute.getName()).append(" is present").toString());
+        }
+
+    }
+
+    public synchronized Uid generateUid()
+    {
+        _uidIndex++;
+        return new Uid((new StringBuilder()).append(_uidIndex).append("").toString());
+    }
+
+    public void delete(ObjectClass objClass, Uid uid, OperationOptions options)
+    {
+        if(!_map.containsKey(uid))
+        {
+            throw new UnknownUidException();
+        } else
+        {
+            _map.remove(uid);
+            return;
+        }
+    }
+
+    public FilterTranslator createFilterTranslator(ObjectClass oclass, OperationOptions options)
+    {
+        return null;
+    }
+
+    public void executeQuery(ObjectClass oclass, String query, ResultsHandler handler, OperationOptions options)
+    {
+        String attrsToGet[] = options.getAttributesToGet();
+        ConnectorObjectBuilder builder;
+        for(Iterator iter = _map.entrySet().iterator(); iter.hasNext(); handler.handle(builder.build()))
+        {
+            java.util.Map.Entry entry = (java.util.Map.Entry)iter.next();
+            builder = new ConnectorObjectBuilder();
+            if(attrsToGet == null)
+            {
+                builder.addAttributes((Collection)entry.getValue());
+            } else
+            {
+                Map map = AttributeUtil.toMap((Collection)entry.getValue());
+                String attributes[] = attrsToGet;
+                int length = attributes.length;
+                for(int i = 0; i < length; i++)
+                {
+                    String attribute = attributes[i];
+                    builder.addAttribute(new Attribute[] {
+                        (Attribute)map.get(attribute)
+                    });
+                }
+
+            }
+            builder.setUid((Uid)entry.getKey());
+        }
+
+    }
+
+    public Uid update(ObjectClass objclass, Set attrs, OperationOptions options)
+    {
+        validateAttributes(objclass, attrs, false);
+        Map attrMap = new HashMap(AttributeUtil.toMap(attrs));
+        Uid uid = (Uid)attrMap.remove(Uid.NAME);
+        if(uid == null)
+            throw new RuntimeException("missing Uid");
+        if(!_map.containsKey(uid))
+        {
+            throw new UnknownUidException();
+        } else
+        {
+            Name name = (Name)attrMap.remove(Name.NAME);
+            Attribute currentPassword = (Attribute)attrMap.remove(OperationalAttributeInfos.CURRENT_PASSWORD);
+            Attribute resetPassword = (Attribute)attrMap.remove(OperationalAttributeInfos.RESET_PASSWORD);
+            Set object = (Set)_map.get(uid);
+            object.addAll(attrMap.values());
+            return uid;
+        }
+    }
+
+    public Schema schema()
+    {
+        return staticSchema();
+    }
+
+    public static Schema staticSchema()
+    {
+        if(_schema != null)
+            return _schema;
+        try
+        {
+            SchemaBuilder schemaBuilder = new SchemaBuilder(DummyConnector.class);
+            Set attributes = new HashSet();
+            attributes.add(OperationalAttributeInfos.CURRENT_PASSWORD);
+            attributes.add(OperationalAttributeInfos.DISABLE_DATE);
+            attributes.add(OperationalAttributeInfos.ENABLE);
+            attributes.add(OperationalAttributeInfos.ENABLE_DATE);
+            attributes.add(OperationalAttributeInfos.LOCK_OUT);
+            attributes.add(OperationalAttributeInfos.PASSWORD);
+            attributes.add(OperationalAttributeInfos.PASSWORD_EXPIRATION_DATE);
+            attributes.add(OperationalAttributeInfos.RESET_PASSWORD);
+            Method setters[] = getAttributeInfoBuilderSetters();
+            AttributeInfoBuilder builder = new AttributeInfoBuilder();
+            attributes.addAll(buildAttributeInfo(builder, setters));
+            schemaBuilder.defineObjectClass("account", attributes);
+            _schema = schemaBuilder.build();
+            return _schema;
+        }
+        catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List buildAttributeInfo(AttributeInfoBuilder builder, Method methods[])
+        throws IllegalArgumentException, IllegalAccessException, InvocationTargetException
+    {
+        List list = new LinkedList();
+        Class supportedTypes[] = (Class[])FrameworkUtil.getAllSupportedAttributeTypes().toArray(new Class[0]);
+        buildAttributeInfo(builder, methods, 0, "", list, supportedTypes, new int[] {
+            0
+        });
+        return list;
+    }
+
+    private static void buildAttributeInfo(AttributeInfoBuilder builder, Method methods[], int index, String name, List list, Class supportedTypes[], int typeIndex[])
+        throws IllegalArgumentException, IllegalAccessException, InvocationTargetException
+    {
+        if(index < methods.length)
+        {
+            methods[index].invoke(builder, new Object[] {
+                Boolean.valueOf(true)
+            });
+            buildAttributeInfo(builder, methods, index + 1, (new StringBuilder()).append(name).append("_T").toString(), list, supportedTypes, typeIndex);
+            methods[index].invoke(builder, new Object[] {
+                Boolean.valueOf(false)
+            });
+            buildAttributeInfo(builder, methods, index + 1, (new StringBuilder()).append(name).append("_F").toString(), list, supportedTypes, typeIndex);
+        } else
+        {
+            Class clazz = supportedTypes[typeIndex[0]];
+            String prefix = clazz.getSimpleName();
+            if(clazz.isArray())
+                prefix = (new StringBuilder()).append(clazz.getComponentType().getSimpleName()).append("Array").toString();
+            builder.setName((new StringBuilder()).append(prefix).append(name).toString().toUpperCase());
+            builder.setType(clazz);
+            AttributeInfo info = builder.build();
+            if((info.isCreateable() || info.isUpdateable()) || !info.isRequired())
+            	if (!(info.isRequired() && clazz.equals(byte[].class)))
+	            {
+	                list.add(info);
+	                typeIndex[0]++;
+	                if(typeIndex[0] == supportedTypes.length)
+	                    typeIndex[0] = 0;
+	            }
+        }
+    }
+
+    private static Method[] getAttributeInfoBuilderSetters()
+    {
+        List setters = new LinkedList();
+        Method methods[] = AttributeInfoBuilder.class.getMethods();
+        int length = methods.length;
+        for(int i = 0; i < length; i++)
+        {
+            Method method = methods[i];
+            if(!method.getName().startsWith("setReturned") && method.getName().startsWith("set") && 
+                method.getParameterTypes().length == 1 && 
+            	(method.getParameterTypes()[0] == Boolean.TYPE || method.getParameterTypes()[0] == Boolean.class))
+                setters.add(method);
+        }
+
+        return (Method[])setters.toArray(new Method[0]);
+    }
+
+    public void executeQuery(ObjectClass x0, Object x1, ResultsHandler x2, OperationOptions x3)
+    {
+        executeQuery(x0, (String)x1, x2, x3);
+    }
+
+    private static int _uidIndex;
+    private static Schema _schema;
+    private static Map _map = new HashMap();
+    private DummyConfiguration _configuration;
+    private DummyConnection _connection;
+
+}
