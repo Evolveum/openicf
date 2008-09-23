6b355ef4b39dbc21298c252b686fd12f20282246
george_hetrick <george_hetrick@sun.com>
2008-09-23 22:01:41 +0000

    some cleanup
    git-svn-id: https://svn.forgerock.org/openicf/trunk@111 05b3e5af-d696-470f-a577-fd7599f74d3c

--- projects/bundles/dummy/src/org/identityconnectors/dummy/DummyConnector.java
+++ projects/bundles/dummy/src/org/identityconnectors/dummy/DummyConnector.java
@@ -53,101 +53,83 @@ import org.identityconnectors.framework.spi.operations.*;
 
 @ConnectorClass(displayNameKey="DummyConnector",configurationClass=DummyConfiguration.class)
 public class DummyConnector
-    implements Connector, CreateOp, DeleteOp, SearchOp, UpdateOp, SchemaOp
+    implements Connector, CreateOp, DeleteOp, SearchOp<String>, UpdateOp, SchemaOp
 {
 
-    public DummyConnector()
-    {
+    public DummyConnector() {
     }
 
-    public void dispose()
-    {
+    public void dispose() {
     }
 
-    public Configuration getConfiguration()
-    {
+    public Configuration getConfiguration() {
         return _configuration;
     }
 
-    public void init(Configuration cfg)
-    {
+    public void init(Configuration cfg) {
         _configuration = (DummyConfiguration)cfg;
         _connection = new DummyConnection();
         _schema = schema();
     }
 
-    public Uid create(ObjectClass objClass, Set attrs, OperationOptions options)
-    {
+    public Uid create(ObjectClass objClass, Set<Attribute> attrs, OperationOptions options) {
         validateAttributes(objClass, attrs, true);
         Uid uid = generateUid();
         _map.put(uid, attrs);
         return uid;
     }
 
-    private void validateAttributes(ObjectClass oclass, Set attrs, boolean checkRequired)
-    {
+    private void validateAttributes(ObjectClass oclass, Set<Attribute> attrs, boolean checkRequired) {
         ObjectClassInfo oci = _schema.findObjectClassInfo(oclass.getObjectClassValue());
-        Map attrMap = new HashMap(AttributeUtil.toMap(attrs));
-        for(Iterator i$ = oci.getAttributeInfo().iterator(); i$.hasNext();)
-        {
-            AttributeInfo attributeInfo = (AttributeInfo)i$.next();
+        Map<String, Attribute> attrMap = new HashMap<String, Attribute>(AttributeUtil.toMap(attrs));
+        for(Iterator<AttributeInfo> i$ = oci.getAttributeInfo().iterator(); i$.hasNext();) {
+            AttributeInfo attributeInfo = i$.next();
             if(checkRequired && attributeInfo.isRequired() && !attrMap.containsKey(attributeInfo.getName()))
                 throw new IllegalArgumentException((new StringBuilder()).append("Required attribute ").append(attributeInfo.getName()).append(" is missing").toString());
             if(!(attributeInfo.isCreateable() || attributeInfo.isUpdateable()) && attrMap.containsKey(attributeInfo.getName()))
                 throw new IllegalArgumentException((new StringBuilder()).append("Non-writeable attribute ").append(attributeInfo.getName()).append(" is present").toString());
         }
 
-        Map ociMap = AttributeInfoUtil.toMap(oci.getAttributeInfo());
-        for(Iterator i$ = attrs.iterator(); i$.hasNext();)
-        {
-            Attribute attribute = (Attribute)i$.next();
+        Map<String, AttributeInfo> ociMap = AttributeInfoUtil.toMap(oci.getAttributeInfo());
+        for(Iterator<Attribute> i$ = attrs.iterator(); i$.hasNext();) {
+            Attribute attribute = i$.next();
             if(!ociMap.containsKey(attribute.getName()))
                 throw new IllegalArgumentException((new StringBuilder()).append("Unknown attribute ").append(attribute.getName()).append(" is present").toString());
         }
 
     }
 
-    public synchronized Uid generateUid()
-    {
+    public synchronized Uid generateUid() {
         _uidIndex++;
         return new Uid((new StringBuilder()).append(_uidIndex).append("").toString());
     }
 
-    public void delete(ObjectClass objClass, Uid uid, OperationOptions options)
-    {
-        if(!_map.containsKey(uid))
-        {
+    public void delete(ObjectClass objClass, Uid uid, OperationOptions options) {
+        if(!_map.containsKey(uid)) {
             throw new UnknownUidException();
-        } else
-        {
+        } else {
             _map.remove(uid);
             return;
         }
     }
 
-    public FilterTranslator createFilterTranslator(ObjectClass oclass, OperationOptions options)
-    {
+    public FilterTranslator<String> createFilterTranslator(ObjectClass oclass, OperationOptions options) {
         return new DummyFilterTranslator();
     }
 
-    public void executeQuery(ObjectClass oclass, String query, ResultsHandler handler, OperationOptions options)
-    {
+    public void executeQuery(ObjectClass oclass, String query, ResultsHandler handler, OperationOptions options) {
         String attrsToGet[] = options.getAttributesToGet();
         ConnectorObjectBuilder builder;
-        for(Iterator iter = _map.entrySet().iterator(); iter.hasNext(); handler.handle(builder.build()))
-        {
-            java.util.Map.Entry entry = (java.util.Map.Entry)iter.next();
+        for(Iterator<Map.Entry<Uid, Set<Attribute>>> iter = _map.entrySet().iterator(); iter.hasNext(); handler.handle(builder.build())) {
+            java.util.Map.Entry<Uid, Set<Attribute>> entry = iter.next();
             builder = new ConnectorObjectBuilder();
-            if(attrsToGet == null)
-            {
-                builder.addAttributes((Collection)entry.getValue());
-            } else
-            {
-                Map map = AttributeUtil.toMap((Collection)entry.getValue());
+            if(attrsToGet == null) {
+                builder.addAttributes(entry.getValue());
+            } else {
+                Map<String, Attribute> map = AttributeUtil.toMap(entry.getValue());
                 String attributes[] = attrsToGet;
                 int length = attributes.length;
-                for(int i = 0; i < length; i++)
-                {
+                for(int i = 0; i < length; i++) {
                     String attribute = attributes[i];
                     builder.addAttribute(new Attribute[] {
                         (Attribute)map.get(attribute)
@@ -160,22 +142,19 @@ public class DummyConnector
 
     }
 
-    public Uid update(ObjectClass objclass, Set attrs, OperationOptions options)
-    {
+    public Uid update(ObjectClass objclass, Set<Attribute> attrs, OperationOptions options) {
         validateAttributes(objclass, attrs, false);
-        Map attrMap = new HashMap(AttributeUtil.toMap(attrs));
+        Map<String, Attribute> attrMap = new HashMap<String, Attribute>(AttributeUtil.toMap(attrs));
         Uid uid = (Uid)attrMap.remove(Uid.NAME);
         if(uid == null)
             throw new RuntimeException("missing Uid");
-        if(!_map.containsKey(uid))
-        {
+        if(!_map.containsKey(uid)) {
             throw new UnknownUidException();
-        } else
-        {
+        } else {
             Name name = (Name)attrMap.remove(Name.NAME);
             Attribute currentPassword = (Attribute)attrMap.remove(OperationalAttributeInfos.CURRENT_PASSWORD);
             Attribute resetPassword = (Attribute)attrMap.remove(OperationalAttributeInfos.RESET_PASSWORD);
-            Set object = (Set)_map.get(uid);
+            Set<Attribute> object = _map.get(uid);
             object.addAll(attrMap.values());
             return uid;
         }
@@ -190,10 +169,9 @@ public class DummyConnector
     {
         if(_schema != null)
             return _schema;
-        try
-        {
+        try {
             SchemaBuilder schemaBuilder = new SchemaBuilder(DummyConnector.class);
-            Set attributes = new HashSet();
+            Set<AttributeInfo> attributes = new HashSet<AttributeInfo>();
             attributes.add(OperationalAttributeInfos.CURRENT_PASSWORD);
             attributes.add(OperationalAttributeInfos.DISABLE_DATE);
             attributes.add(OperationalAttributeInfos.ENABLE);
@@ -208,29 +186,23 @@ public class DummyConnector
             schemaBuilder.defineObjectClass("account", attributes);
             _schema = schemaBuilder.build();
             return _schema;
-        }
-        catch(Exception e)
-        {
+        } catch(Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static List buildAttributeInfo(AttributeInfoBuilder builder, Method methods[])
-        throws IllegalArgumentException, IllegalAccessException, InvocationTargetException
-    {
-        List list = new LinkedList();
-        Class supportedTypes[] = (Class[])FrameworkUtil.getAllSupportedAttributeTypes().toArray(new Class[0]);
-        buildAttributeInfo(builder, methods, 0, "", list, supportedTypes, new int[] {
+    private static List<AttributeInfo> buildAttributeInfo(AttributeInfoBuilder builder, Method methods[])
+        throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+        List<AttributeInfo> list = new LinkedList<AttributeInfo>();
+        buildAttributeInfo(builder, methods, 0, "", list, _supportedTypes, new int[] {
             0
         });
         return list;
     }
 
-    private static void buildAttributeInfo(AttributeInfoBuilder builder, Method methods[], int index, String name, List list, Class supportedTypes[], int typeIndex[])
-        throws IllegalArgumentException, IllegalAccessException, InvocationTargetException
-    {
-        if(index < methods.length)
-        {
+    private static void buildAttributeInfo(AttributeInfoBuilder builder, Method methods[], int index, String name, List<AttributeInfo> list, Class<? extends Object> supportedTypes[], int typeIndex[])
+        throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+        if(index < methods.length) {
             methods[index].invoke(builder, new Object[] {
                 Boolean.valueOf(true)
             });
@@ -244,33 +216,30 @@ public class DummyConnector
                 Boolean.valueOf(false)
             });
             buildAttributeInfo(builder, methods, index + 1, (new StringBuilder()).append(name).append("_F").toString(), list, supportedTypes, typeIndex);
-        } else
-        {
-            Class clazz = supportedTypes[typeIndex[0]];
+        } else {
+            Class<? extends Object> clazz = supportedTypes[typeIndex[0]];
             String prefix = clazz.getSimpleName();
             if(clazz.isArray())
                 prefix = (new StringBuilder()).append(clazz.getComponentType().getSimpleName()).append("Array").toString();
             builder.setName((new StringBuilder()).append(prefix).append(name).toString().toUpperCase());
             builder.setType(clazz);
             AttributeInfo info = builder.build();
-            if((info.isCreateable() || info.isUpdateable()) || !info.isRequired())
-            	if (!(info.isRequired() && clazz.equals(byte[].class)))
-	            {
+            if((info.isCreateable() || info.isUpdateable()) || !info.isRequired()) {
+            	if (!(info.isRequired() && clazz.equals(byte[].class))) {
 	                list.add(info);
 	                typeIndex[0]++;
 	                if(typeIndex[0] == supportedTypes.length)
 	                    typeIndex[0] = 0;
 	            }
+            }
         }
     }
 
-    private static Method[] getAttributeInfoBuilderSetters()
-    {
-        List setters = new LinkedList();
+    private static Method[] getAttributeInfoBuilderSetters() {
+        List<Method> setters = new LinkedList<Method>();
         Method methods[] = AttributeInfoBuilder.class.getMethods();
         int length = methods.length;
-        for(int i = 0; i < length; i++)
-        {
+        for(int i = 0; i < length; i++) {
             Method method = methods[i];
             if(!method.getName().startsWith("setReturned") && method.getName().startsWith("set") && 
                 method.getParameterTypes().length == 1 && 
@@ -281,14 +250,10 @@ public class DummyConnector
         return (Method[])setters.toArray(new Method[0]);
     }
 
-    public void executeQuery(ObjectClass x0, Object x1, ResultsHandler x2, OperationOptions x3)
-    {
-        executeQuery(x0, (String)x1, x2, x3);
-    }
-
     private static int _uidIndex;
     private static Schema _schema;
-    private static Map _map = new HashMap();
+    private static Class<? extends Object> _supportedTypes[] = FrameworkUtil.getAllSupportedAttributeTypes().toArray(new Class[0]);
+    private static Map<Uid, Set<Attribute>> _map = new HashMap<Uid, Set<Attribute>>();
     private DummyConfiguration _configuration;
     private DummyConnection _connection;
 
