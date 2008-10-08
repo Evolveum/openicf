0f32e0d6446b2dc6971a56d33c4e70f41a60fa11
wdroste <wdroste@sun.com>
2008-10-08 16:49:24 +0000

    Issue #281 -Added 'ConnectorAttributeAccessor' and returned 'this' for most of the builders.
    git-svn-id: https://svn.forgerock.org/openicf/trunk@226 05b3e5af-d696-470f-a577-fd7599f74d3c

--- projects/dotnet/Framework/CommonObjects.cs
+++ projects/dotnet/Framework/CommonObjects.cs
@@ -742,11 +742,13 @@ namespace Org.IdentityConnectors.Framework.Common.Objects
             }
         }
         
-        public void AddValue(params Object [] args) {
+        public ConnectorAttributeBuilder AddValue(params Object [] args) {
             AddValuesInternal(args);
+            return this;
         }
-        public void AddValue(ICollection<Object> values) {
+        public ConnectorAttributeBuilder AddValue(ICollection<Object> values) {
             AddValuesInternal(values);
+            return this;
         }
         
         public ConnectorAttribute Build() {
@@ -1130,39 +1132,44 @@ namespace Org.IdentityConnectors.Framework.Common.Objects
          * Takes all the attribute from a {@link ConnectorObject} and add/overwrite
          * the current attributes.
          */
-        public void Add(ConnectorObject obj) {
+        public ConnectorObjectBuilder Add(ConnectorObject obj) {
             // simply add all the attributes it will include (Uid, ObjectClass..)
             foreach (ConnectorAttribute attr in obj.GetAttributes()) {
                AddAttribute(attr);
             }
             ObjectClass = obj.ObjectClass;
+            return this;
         }
         
-        public void AddAttribute(params ConnectorAttribute [] attrs) {
+        public ConnectorObjectBuilder AddAttribute(params ConnectorAttribute [] attrs) {
             ValidateParameter(attrs, "attrs");
             foreach (ConnectorAttribute a in attrs) {
                 //DONT use Add - it throws exceptions if already there
                 _attributes[a.Name] = a;
             }
+            return this;
         }
-        public void AddAttributes(ICollection<ConnectorAttribute> attrs) {
+        public ConnectorObjectBuilder AddAttributes(ICollection<ConnectorAttribute> attrs) {
             ValidateParameter(attrs, "attrs");
             foreach (ConnectorAttribute a in attrs) {
                 _attributes[a.Name] = a;
             }
+            return this;
         }
         /**
          * Adds values to the attribute.
          */
-        public void AddAttribute(String name, params object [] objs) {
+        public ConnectorObjectBuilder AddAttribute(String name, params object [] objs) {
             AddAttribute(ConnectorAttributeBuilder.Build(name, objs));
+            return this;
         }
 
         /**
          * Adds each object in the collection.
          */
-        public void AddAttribute(String name, ICollection<object> obj) {
+        public ConnectorObjectBuilder AddAttribute(String name, ICollection<object> obj) {
             AddAttribute(ConnectorAttributeBuilder.Build(name, obj));
+            return this;
         }
         public ConnectorObject Build() {
             // check that there are attributes to return..
@@ -1633,18 +1640,20 @@ namespace Org.IdentityConnectors.Framework.Common.Objects
         /**
          * Add each {@link AttributeInfo} object to the {@link ObjectClassInfo}.
          */
-        public void AddAttributeInfo(ConnectorAttributeInfo info) {
+        public ObjectClassInfoBuilder AddAttributeInfo(ConnectorAttributeInfo info) {
             if (_info.ContainsKey(info.Name)) {
                 const string MSG = "ConnectorAttributeInfo of name ''{0}'' already exists!";
                 throw new ArgumentException(String.Format(MSG, info.Name));
             }
             _info[info.Name] = info;
+            return this;
         }
     
-        public void AddAllAttributeInfo(ICollection<ConnectorAttributeInfo> info) {
+        public ObjectClassInfoBuilder AddAllAttributeInfo(ICollection<ConnectorAttributeInfo> info) {
             foreach (ConnectorAttributeInfo cainfo in info) {
                 AddAttributeInfo(cainfo);
             }
+        	return this;
         }
     
         public ObjectClassInfo Build() {
@@ -2889,24 +2898,26 @@ namespace Org.IdentityConnectors.Framework.Common.Objects
          * type types that the framework can serialize.
          * @see ObjectSerializerFactory for a list of supported types.
          */
-        public void AddScriptArgument(String name, Object value) {
+        public ScriptContextBuilder AddScriptArgument(String name, Object value) {
             if ( name == null ) {
                 throw new ArgumentException("Argument 'name' cannot be null.");
             }
             //don't validate value here - we do that implicitly when
             //we clone in the constructor of ScriptRequest
             _scriptArguments[name] = value;
+            return this;
         }
         
         /**
          * Removes the given script argument.
          * @param name The name of the argument. Must not be null.
          */
-        public void RemoveScriptArgument(String name) {
+        public ScriptContextBuilder RemoveScriptArgument(String name) {
             if ( name == null ) {
                 throw new ArgumentException("Argument 'name' cannot be null.");
             }
             _scriptArguments.Remove(name);
+            return this;
         }
         
         /**
@@ -3295,4 +3306,230 @@ namespace Org.IdentityConnectors.Framework.Common.Objects
         }
     }
     #endregion
+
+    #region ConnectorAttributesAccessor
+    /**
+	 * Attributes Accessor convenience methods for accessing attributes.
+	 * 
+	 * This class wraps a set of attributes to make lookup faster than the
+	 * {@link AttributeUtil#find(String, Set)} method, since that method must
+	 * re-create the map each time.
+	 * 
+	 * @author Warren Strange
+	 */
+	public class ConnectorAttributesAccessor {
+	
+	    ICollection<ConnectorAttribute> _attrs;
+	    IDictionary<String, ConnectorAttribute> _attrMap;
+	
+	    public ConnectorAttributesAccessor(ICollection<ConnectorAttribute> attrs) {
+	        _attrs = attrs;
+	        _attrMap = ConnectorAttributeUtil.ToMap(attrs);
+	    }
+	
+	    /**
+	     * Find the named attribute
+	     * 
+	     * @param name -
+	     *            the attribute name to search for
+	     * @return the Attribute, or null if not found.
+	     */
+	    public ConnectorAttribute Find(String name) {
+	        return CollectionUtil.GetValue(_attrMap, name, null);
+	    }
+	
+	    /**
+	     * Get the {@link Name} attribute from the set of attributes.
+	     * 
+	     * @return the {@link Name} attribute in the set.
+	     */
+	    public Name GetName() {
+	        return (Name) Find(Name.NAME);
+	    }
+	
+	    /**
+	     * Return the enabled status of the account.
+	     * 
+	     * If the ENABLE operational attribute is present, it's value takes
+	     * precedence over the current value. If it is missing, the currentlyEnabled
+	     * status is returned instead.
+	     * 
+	     * @param dflt
+	     *            the default state if enable is not found.
+	     * @return true if the account is enabled, false otherwise
+	     */
+	    public bool GetEnabled(bool dflt) {
+	        bool e = dflt;
+	        ConnectorAttribute enable = Find(OperationalAttributes.ENABLE_NAME);
+	        if (enable != null) {
+	            e = ConnectorAttributeUtil.GetBooleanValue(enable).Value;
+	        }
+	        return e;
+	    }
+	
+	    /**
+	     * Get the password as a GuardeString
+	     * 
+	     * @return the password as a guarded String
+	     */
+	    public GuardedString GetPassword() {
+	        ConnectorAttribute a = Find(OperationalAttributes.PASSWORD_NAME);
+	        return a == null ? null : ConnectorAttributeUtil.GetGuardedStringValue(a);
+	    }
+	
+	    /**
+	     * Return a list of attributes
+	     * 
+	     * @param name -
+	     *            name of attribute to search for.
+	     * 
+	     * @return The List (generic object) iff it exists otherwise null.
+	     */
+	    public IList<Object> FindList(String name) {
+	        ConnectorAttribute a = Find(name);
+	        return (a == null) ? null : a.Value;
+	    }
+	
+	    /**
+	     * Return the multivalued attribute as a list of strings. This will throw a
+	     * ClassCastException if the underlying attribute list is not of type
+	     * String.
+	     * 
+	     * @param name
+	     *            the name of the attribute to search for
+	     * @return a List of String values for the attribute
+	     */
+	    public IList<String> FindStringList(String name) {
+	        IList<Object> l = FindList(name);
+	        if (l != null) {
+	            IList<String> ret = new List<String>(l.Count);
+	            foreach (object o in l) {
+	                ret.Add((String) o);
+	            }
+	            return ret;
+	        }
+	        return null;
+	    }
+	
+	    /**
+	     * Determines if the set as the attribute specified.
+	     * 
+	     * @param name
+	     *            attribute name
+	     * @return true if the named attribute exists, false otherwise
+	     */
+	    public bool HasAttribute(String name) {
+	        return Find(name) != null;
+	    }
+	
+	    /**
+	     * Get the string value from the specified (single-valued) attribute.
+	     * 
+	     * @param name
+	     *            Attribute from which to retrieve the long value.
+	     * @return null if the value is null otherwise the long value for the
+	     *         attribute.
+	     * @throws ClassCastException
+	     *             iff the object in the attribute is not an long.
+	     * @throws IllegalArgumentException
+	     *             iff the attribute is a multi-valued (rather than
+	     *             single-valued).
+	     */
+	    public String FindString(String name) {
+	        ConnectorAttribute a = Find(name);
+	        return a == null ? null : ConnectorAttributeUtil.GetStringValue(a);
+	    }
+	
+	    /**
+	     * Get the integer value from the specified (single-valued) attribute.
+	     * 
+	     * @param name
+	     *            Attribute from which to retrieve the long value.
+	     * @return null if the value is null otherwise the long value for the
+	     *         attribute.
+	     * @throws ClassCastException
+	     *             iff the object in the attribute is not an long.
+	     * @throws IllegalArgumentException
+	     *             iff the attribute is a multi-valued (rather than
+	     *             single-valued).
+	     */
+	    public int? FindInteger(String name) {
+	        ConnectorAttribute a = Find(name);
+	        return (a == null) ? null : ConnectorAttributeUtil.GetIntegerValue(a);
+	    }
+	
+	    /**
+	     * Get the long value from the specified (single-valued) attribute.
+	     * 
+	     * @param name
+	     *            Attribute from which to retrieve the long value.
+	     * @return null if the value is null otherwise the long value for the
+	     *         attribute.
+	     * @throws ClassCastException
+	     *             iff the object in the attribute is not an long.
+	     * @throws IllegalArgumentException
+	     *             iff the attribute is a multi-valued (rather than
+	     *             single-valued).
+	     */
+	    public long? FindLong(String name) {
+	        ConnectorAttribute a = Find(name);
+	        return a == null ? null : ConnectorAttributeUtil.GetLongValue(a);
+	    }
+	
+	    /**
+	     * Get the date value from the specified (single-valued) attribute that
+	     * contains a long.
+	     * 
+	     * @param name
+	     *            Attribute from which to retrieve the date value.
+	     * @return null if the value is null otherwise the date value for the
+	     *         attribute.
+	     * @throws ClassCastException
+	     *             iff the object in the attribute is not an long.
+	     * @throws IllegalArgumentException
+	     *             iff the attribute is a multi-valued (rather than
+	     *             single-valued).
+	     */
+	    public DateTime? FindDateTime(String name) {
+	        ConnectorAttribute a = Find(name);
+	        return a == null ? null : ConnectorAttributeUtil.GetDateTimeValue(a);
+	    }
+	
+	    /**
+	     * Get the integer value from the specified (single-valued) attribute.
+	     * 
+	     * @param name
+	     *            Attribute from which to retrieve the integer value.
+	     * @return null if the value is null otherwise the integer value for the
+	     *         attribute.
+	     * @throws ClassCastException
+	     *             iff the object in the attribute is not an integer.
+	     * @throws IllegalArgumentException
+	     *             iff the attribute is a multi-valued (rather than
+	     *             single-valued)..
+	     */
+	    public double? FindDouble(String name) {
+	        ConnectorAttribute a = Find(name);
+	        return a == null ? null : ConnectorAttributeUtil.GetDoubleValue(a);
+	    }
+
+	    /**
+	     * Get the boolean value from the specified (single-valued) attribute.
+	     * 
+	     * @param name
+	     *            Attribute from which to retrieve the boolean value.
+	     * @return null if the value is null otherwise the boolean value for the
+	     *         attribute.
+	     * @throws ClassCastException
+	     *             iff the object in the attribute is not an {@link Boolean}.
+	     * @throws IllegalArgumentException
+	     *             iff the attribute is a multi-valued (rather than
+	     *             single-valued).
+	     */
+	    public bool? FindBoolean(String name) {
+	        ConnectorAttribute a = Find(name);
+	        return a == null ? null : ConnectorAttributeUtil.GetBooleanValue(a);
+	    }
+	}
+	#endregion
 }
