59e2e34ef22f4ef777fddccd1c4c8b6843af02a3
roman_kitko <roman_kitko@sun.com>
2009-01-12 10:36:48 +0000

    Add oracle connector
    git-svn-id: https://svn.forgerock.org/openicf/trunk@2225 05b3e5af-d696-470f-a577-fd7599f74d3c

---
+++ projects/bundles/oracle/src/org/identityconnectors/oracle/OracleConfiguration.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.identityconnectors.oracle;
+
+import org.identityconnectors.framework.spi.AbstractConfiguration;
+
+/**
+ * @author kitko
+ *
+ */
+public class OracleConfiguration extends AbstractConfiguration {
+
+    /**
+     * 
+     */
+    public OracleConfiguration() {
+    }
+
+    @Override
+    public void validate() {
+    }
+
+}
