2c29d7136ec0cb7bfadebb85da490cd7f26bbb58
george_hetrick <george_hetrick@sun.com>
2009-02-23 19:01:29 +0000

    remove identifying info.
    git-svn-id: https://svn.forgerock.org/openicf/trunk@3938 05b3e5af-d696-470f-a577-fd7599f74d3c

--- projects/bundles/vms/src/org/identityconnectors/vms/VmsConnectorTests.java
+++ projects/bundles/vms/src/org/identityconnectors/vms/VmsConnectorTests.java
@@ -96,9 +96,9 @@ public class VmsConnectorTests {
 
     @BeforeClass
     public static void before() {
-        HOST_NAME         = "idm090.central.sun.com";//TestHelpers.getProperty("HOST_NAME", null);
-        SYSTEM_PASSWORD   = "gough4130a";//TestHelpers.getProperty("SYSTEM_PASSWORD", null);
-        SYSTEM_USER       = "system";//TestHelpers.getProperty("SYSTEM_USER", null);
+        HOST_NAME         = TestHelpers.getProperty("HOST_NAME", null);
+        SYSTEM_PASSWORD   = TestHelpers.getProperty("SYSTEM_PASSWORD", null);
+        SYSTEM_USER       = TestHelpers.getProperty("SYSTEM_USER", null);
         Assert.assertNotNull("HOST_NAME must be specified", HOST_NAME);
         Assert.assertNotNull("SYSTEM_PASSWORD must be specified", SYSTEM_PASSWORD);
         Assert.assertNotNull("SYSTEM_USER must be specified", SYSTEM_USER);
