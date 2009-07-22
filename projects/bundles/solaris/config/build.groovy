029817c0cdd9157dc53e30ddd59779b442e1b621
davidadam <davidadam@sun.com>
2009-07-22 16:46:06 +0000

    configuring contract tests
    git-svn-id: https://svn.forgerock.org/openicf/trunk@4936 05b3e5af-d696-470f-a577-fd7599f74d3c

--- projects/bundles/solaris/config/build.groovy
+++ projects/bundles/solaris/config/build.groovy
@@ -17,4 +17,88 @@ testsuite {
   bundleName=System.getProperty("bundleName")
   bundleVersion=System.getProperty("bundleVersion")
   connectorName="org.identityconnectors.solaris.SolarisConnector"
-}
+
+  Validate.invalidConfig = [
+    [port: null],
+    [password: null],
+    [connectionType: 'boobar']
+  ]//Validate
+
+  Test.invalidConfig = [
+    [password: "nonsensePassword123456"], 
+    [userName: "nonsenseUserName123456"]
+  ]//Test
+
+  Schema {
+    oclasses = ['__ACCOUNT__', '__GROUP__']
+    attributes {
+      __GROUP__.oclasses = ['__NAME__' /* TODO, not yet decided wether to use the adapter's 'groupName' or rather the Framework's constant. */, '__UID__', 'gid', 'users']
+      __ACCOUNT__.oclasses = ['__NAME__', 'dir', 'shell', 'group', 'secondary_group',
+        '__UID__', 'expire', 'inactive', 'comment', 'time_last_login',
+        'authorization', 'profile', 'role', 'max', 'min', 'warn', 'lock', 
+        '__PASSWORD__' /* TODO extra attribute that wasn't in the schema of Adatper -- is it OK? */, 
+         'force_change'
+      ] //__ACCOUNT__.oclasses
+    }//attributes
+
+    attrTemplate = [
+      type: String.class,
+      readable: true,
+      createable: true,
+      updateable: true,
+      required: false,
+      multiValue: false,
+      returnedByDefault: true
+    ]// attrTemplate
+  
+    attrRequiredTemplate = [
+      type: String.class,
+      readable: true,
+      createable: true,
+      updateable: true,
+      required: true,
+      multiValue: false,
+      returnedByDefault: true
+    ]
+
+    attrPasswdTemplate = [
+      type: org.identityconnectors.common.security.GuardedString.class,
+      readable: false,
+      createable: true,
+      updateable: true,
+      required: false,
+      multiValue: false,
+      returnedByDefault: false
+    ]
+  
+
+    gid.attribute.__GROUP__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    __NAME__.attribute.__GROUP__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    users.attribute.__GROUP__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    __UID__.attribute.__GROUP__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+
+    inactive.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    min.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    max.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    secondary_group.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    group.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    __NAME__.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    expire.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    warn.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    dir.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    comment.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    __UID__.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    lock.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    time_last_login.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    authorization.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    profile.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    __PASSWORD__.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrPasswdTemplate")
+    role.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    force_change.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+    shell.attribute.__ACCOUNT__.oclasses = Lazy.get("testsuite.Schema.attrTemplate")
+
+    //TODO operations = []...
+  } //Schema
+
+}// testsuite
+
