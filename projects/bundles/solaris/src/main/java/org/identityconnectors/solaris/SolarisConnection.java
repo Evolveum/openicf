7824e41fd106cb33cc51b786933b2c47caeb83b7
davidadam <davidadam@sun.com>
2009-08-27 09:39:59 +0000

    Bugfix: turned off user keyboard input echoing on the terminal.
    git-svn-id: https://svn.forgerock.org/openicf/trunk@5124 05b3e5af-d696-470f-a577-fd7599f74d3c

--- projects/bundles/solaris/src/main/java/org/identityconnectors/solaris/SolarisConnection.java
+++ projects/bundles/solaris/src/main/java/org/identityconnectors/solaris/SolarisConnection.java
@@ -92,6 +92,18 @@ public class SolarisConnection {
             // .getPort());
             //break;
         }
+        
+        try {
+            waitFor(_configuration.getRootShellPrompt());
+            /*
+             * turn off the echoing of keyboard input on the resource.
+             * Saves bandwith too.
+             */
+            send("stty -echo");
+            waitFor(_configuration.getRootShellPrompt());
+        } catch (Exception e) {
+            throw ConnectorException.wrap(e);
+        }
     }
 
     /**
