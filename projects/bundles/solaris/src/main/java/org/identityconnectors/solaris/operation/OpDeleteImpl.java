37f0908405ff5e6c4e4374586bc80d0a09f88d47
davidadam <davidadam@sun.com>
2009-10-29 17:11:25 +0000

    [NIS] finished delete operation implementation, TODO test.
    git-svn-id: https://svn.forgerock.org/openicf/trunk@5439 05b3e5af-d696-470f-a577-fd7599f74d3c

--- projects/bundles/solaris/src/main/java/org/identityconnectors/solaris/operation/OpDeleteImpl.java
+++ projects/bundles/solaris/src/main/java/org/identityconnectors/solaris/operation/OpDeleteImpl.java
@@ -22,6 +22,7 @@
  */
 package org.identityconnectors.solaris.operation;
 
+import org.identityconnectors.common.CollectionUtil;
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.framework.common.exceptions.ConnectorException;
 import org.identityconnectors.framework.common.exceptions.UnknownUidException;
@@ -30,6 +31,8 @@ import org.identityconnectors.framework.common.objects.OperationOptions;
 import org.identityconnectors.framework.common.objects.Uid;
 import org.identityconnectors.solaris.SolarisConnector;
 import org.identityconnectors.solaris.SolarisUtil;
+import org.identityconnectors.solaris.operation.nis.CommonNIS;
+import org.identityconnectors.solaris.operation.nis.OpDeleteNISImpl;
 
 public class OpDeleteImpl extends AbstractOp {
 
@@ -54,27 +57,68 @@ public class OpDeleteImpl extends AbstractOp {
         
         _log.info("delete(''{0}'')", accountId);
         
-        // USERDEL accountId
-        final String command = getConnection().buildCommand("userdel", ((getConfiguration().isDelHomeDir()) ? "-r" : ""), accountId);
-        
-        try {
-            String output = getConnection().executeCommand(command);
-            if (output.contains("does not exist") || output.contains("nknown user")) {
-                throw new UnknownUidException("Unknown Uid: " + accountId);
-            } else if (output.contains("ERROR")) {
-                throw new ConnectorException("ERROR during delete operation for user '" + accountId + "', buffer content: <" + output + ">");
-            }
-            
-            output = getConnection().executeCommand("echo $?");
-            if (!output.equals("0")) {
-                throw new ConnectorException("ERROR during delete operation for user '" + accountId + "', buffer content: <" + output + ">" + " the error code returned from userdel was not '0'");
-            }
-        } catch (Exception ex) {
-            throw ConnectorException.wrap(ex);
+        if (SolarisUtil.isNis(getConnection())) {
+            invokeNISDelete(accountId);
+        } else {
+            invokeNativeDelete(accountId);
         }
 
         // TODO add handling of exceptions: existing user, etc.
         _log.ok("userdel(''{0}'')", accountId);
 
+    }
+
+    /**
+     * NIS Delete implementation.
+     * 
+     * Compare with Native delete operation: {@see OpDeleteImpl#invokeNativeDelete(String)}
+     */
+    private void invokeNISDelete(String accountId) {
+        // If the password source file is in /etc then use the native
+        // utilities
+        if (CommonNIS.isDefaultNisPwdDir(getConnection())) {
+            invokeNativeDelete(accountId);
+            /*
+             * START SUDO
+             * TODO in adapter, SRA#getDeleteNISUserScript sudo is missing (file another bug?)
+             */
+            getConnection().doSudoStart();
+            try {
+                CommonNIS.addNISMake("passwd", getConnection());
+            } finally {
+                /*
+                 * END SUDO
+                 */
+                getConnection().doSudoReset();
+            }
+        } else {
+            OpDeleteNISImpl.performNIS(accountId, getConnection());
+        }
+    }
+
+    /**
+     * implementation of the Native Delete operation.
+     * 
+     * Compare with NIS implementation: {@see OpDeleteImpl#invokeNISDelete(String)} 
+     */
+    private void invokeNativeDelete(final String accountId) {
+        // USERDEL accountId
+        final String command = getConnection().buildCommand("userdel", ((getConfiguration().isDelHomeDir()) ? "-r" : ""), accountId);
+        
+        try {
+            String output = getConnection().executeCommand(command, CollectionUtil.newSet("ERROR"));
+            if (output.contains("does not exist") || output.contains("nknown user")) {
+                throw new UnknownUidException("Unknown Uid: " + accountId);
+            } else if (output.contains("ERROR")) {
+                throw new ConnectorException("Error deleting user '" + accountId + "'.");
+            }
+            
+            output = getConnection().executeCommand("echo $?");
+            if (!output.equals("0")) {
+                throw new ConnectorException("Error deleting user '" + accountId + "'.");
+            }
+        } catch (Exception ex) {
+            throw ConnectorException.wrap(ex);
+        }
     }
 }
---
+++ projects/bundles/solaris/src/main/java/org/identityconnectors/solaris/operation/nis/OpDeleteNISImpl.java
@@ -0,0 +1,121 @@
+/*
+ * ====================
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2008-2009 Sun Microsystems, Inc. All rights reserved.     
+ * 
+ * The contents of this file are subject to the terms of the Common Development 
+ * and Distribution License("CDDL") (the "License").  You may not use this file 
+ * except in compliance with the License.
+ * 
+ * You can obtain a copy of the License at 
+ * http://IdentityConnectors.dev.java.net/legal/license.txt
+ * See the License for the specific language governing permissions and limitations 
+ * under the License. 
+ * 
+ * When distributing the Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at identityconnectors/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the fields 
+ * enclosed by brackets [] replaced by your own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * ====================
+ */
+package org.identityconnectors.solaris.operation.nis;
+
+import java.util.Collections;
+
+import org.identityconnectors.common.CollectionUtil;
+import org.identityconnectors.solaris.SolarisConnection;
+
+public class OpDeleteNISImpl extends AbstractNISOp {
+
+    public static void performNIS(String accountId, SolarisConnection connection) {
+
+        try {
+            connection.doSudoStart();
+            
+            try {
+                connection.executeMutexAcquireScript(pwdMutexFile, tmpPwdMutexFile, pwdPidFile);
+                
+                deleteImpl(accountId, connection);
+            } finally {
+                connection.executeMutexReleaseScript(pwdMutexFile);
+            }
+        } finally {
+            connection.doSudoReset();
+        }
+    }
+
+    private static void deleteImpl(String accountId,
+            SolarisConnection connection) {
+
+        final String pwddir = CommonNIS.getNisPwdDir(connection);
+        final String pwdFile = pwddir + "/passwd";
+        final String shadowFile = pwddir + "/shadow";
+        final String removeTmpFilesScript = CommonNIS.getRemovePwdTmpFiles(connection);
+        
+        
+        
+        final String getOwner = initGetOwner(pwdFile);
+        final String workScript = initWorkScript(accountId, pwdFile, connection);
+        connection.executeCommand(getOwner);
+        connection.executeCommand(workScript);
+        // two extra 'waitFor(rootShellPrompt)'-s are needed, because they are produced by the script
+        // waitFor #1
+        connection.executeCommand(null, Collections.<String>emptySet());
+        // waitFor #2 -- it should not result in ERROR.
+        connection.executeCommand(null/*no command sent on purpose*/, CollectionUtil.newSet("ERROR"));
+        connection.executeCommand(removeTmpFilesScript);
+        
+        if (connection.getConfiguration().isNisShadow()) {
+            final String getOwnerShadow = initGetOwner(shadowFile);
+            final String workScriptShadow = initWorkScript(accountId, shadowFile, connection);
+            connection.executeCommand(getOwnerShadow);
+            connection.executeCommand(workScriptShadow);
+            // two extra 'waitFor(rootShellPrompt)'-s are needed, because they are produced by the script
+            // waitFor #1
+            connection.executeCommand(null, Collections.<String>emptySet());
+            // waitFor #2
+            connection.executeCommand(null, Collections.<String>emptySet());
+        }
+        
+        connection.executeCommand(removeTmpFilesScript);
+        
+        // The user has to be removed from the NIS database
+        // TODO this comment doesn't make much sense (from adapter)
+        CommonNIS.addNISMake("passwd", connection);
+    }
+
+    private static String initWorkScript(final String accountId, final String pwdFile, final SolarisConnection connection) {
+        final String cpCmd = connection.buildCommand("cp");
+        final String mvCmd = connection.buildCommand("mv");
+        final String chownCmd = connection.buildCommand("chown");
+        final String grepCmd = connection.buildCommand("grep");
+        
+        StringBuilder workScript = new StringBuilder();
+        workScript.append(
+            cpCmd + "-p " + pwdFile + " " + tmpPwdfile1 + "; "); 
+        workScript.append(
+            grepCmd + "-v \"^" + accountId + ":\" " + pwdFile + " > " + tmpPwdfile2 + "\n");
+        workScript.append(
+            cpCmd + "-p " + tmpPwdfile2 + " " + tmpPwdfile1 + "; ");
+        workScript.append(
+            mvCmd + "-f " + tmpPwdfile1 + " " + pwdFile + "\n");
+        workScript.append(
+            chownCmd + "$OWNER:$GOWNER " + pwdFile);
+        return workScript.toString();
+    }
+
+    private static String initGetOwner(final String pwdFile) {
+        // Add script to remove entry in passwd file
+        StringBuilder getOwner = new StringBuilder();
+        getOwner.append("OWNER=`ls -l ");
+        getOwner.append(pwdFile);
+        getOwner.append(" | awk '{ print $3 }'`; ");
+        getOwner.append("GOWNER=`ls -l ");
+        getOwner.append(pwdFile);
+        getOwner.append(" | awk '{ print $4 }'`");
+        return getOwner.toString();
+    }
+
+}
