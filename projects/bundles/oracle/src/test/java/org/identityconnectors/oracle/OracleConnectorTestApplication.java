b34f65c71ba19f4b3d21b077a6cd0b2a393b6ee6
roman_kitko <roman_kitko@sun.com>
2010-03-12 15:22:37 +0000

    Add more samples to show usage of oracle connector
    git-svn-id: https://svn.forgerock.org/openicf/trunk@5726 05b3e5af-d696-470f-a577-fd7599f74d3c

--- projects/bundles/oracle/src/test/java/org/identityconnectors/oracle/OracleConnectorTestApplication.java
+++ projects/bundles/oracle/src/test/java/org/identityconnectors/oracle/OracleConnectorTestApplication.java
@@ -45,6 +45,9 @@ import org.identityconnectors.framework.common.objects.AttributeBuilder;
 import org.identityconnectors.framework.common.objects.ConnectorObject;
 import org.identityconnectors.framework.common.objects.Name;
 import org.identityconnectors.framework.common.objects.ObjectClass;
+import org.identityconnectors.framework.common.objects.OperationOptions;
+import org.identityconnectors.framework.common.objects.OperationOptionsBuilder;
+import org.identityconnectors.framework.common.objects.OperationalAttributes;
 import org.identityconnectors.framework.common.objects.ResultsHandler;
 import org.identityconnectors.framework.common.objects.Uid;
 import org.identityconnectors.framework.common.objects.filter.FilterBuilder;
@@ -52,49 +55,83 @@ import org.identityconnectors.test.common.PropertyBag;
 import org.identityconnectors.test.common.TestHelpers;
 
 /**
- * This class shows how we can use the connector in any application using the framework.
- * To run the class, you must first build the connector using ant to have jars file in dist directory.
+ * This class shows how we can use the connector in any application using the framework. To run the class, you must first build the connector using
+ * ant to have jars file in dist directory.
  * @author kitko
- *
  */
 public class OracleConnectorTestApplication {
-    
-    public static void main(String[] args) throws MalformedURLException{
-       //Here we find our connector jar file 
-       URL[] jarFiles = findConnectorsJar(args);
-       //Creates the local manager instead of RemoteManager
-       ConnectorInfoManager manager = ConnectorInfoManagerFactory.getInstance().getLocalManager(jarFiles);
-       //Just for presentation purpose print all connectors
-       printAvailableConnectors(manager.getConnectorInfos());
-       //Find key for oracle connector
-       ConnectorKey key = findOracleConnectorKey(manager.getConnectorInfos());
-       ConnectorInfo connectorInfo = manager.findConnectorInfo(key);
-       //Create the configuration
-       APIConfiguration config = connectorInfo.createDefaultAPIConfiguration();
-       //Now we can set the connect properties of connector
-       ConfigurationProperties configProps = config.getConfigurationProperties();
-       printConfigProperties(configProps);
-       //Here we could harcode the connect properties
-       //configProps.setPropertyValue("host", "myHost");
-       //But we would use TestHelpers property bag
-       setConnectProperties(configProps);
-       ConnectorFacade facade = ConnectorFacadeFactory.getInstance().newInstance(config);
-       printSupportedOperations(facade);
-       
-       facade.validate();
-       facade.test();
-       
-       findAllUsers(facade);
-       findSpecificUsers(facade);
-       createUser(facade);
-       authenticateUser(facade);
-       
-       
-        
+
+    public static void main(String[] args) throws MalformedURLException {
+        // Here we find our connectors jar files
+        URL[] jarFiles = findConnectorsJar(args);
+        // Creates the local manager instead of RemoteManager
+        ConnectorInfoManager manager = ConnectorInfoManagerFactory.getInstance().getLocalManager(jarFiles);
+        // Just for presentation purpose print all connectors available
+        printAvailableConnectors(manager.getConnectorInfos());
+        // Find key for oracle connector
+        ConnectorKey key = findOracleConnectorKey(manager.getConnectorInfos());
+        ConnectorInfo connectorInfo = manager.findConnectorInfo(key);
+        // Create the configuration
+        APIConfiguration config = connectorInfo.createDefaultAPIConfiguration();
+        // Now we can set the connect properties of connector
+        ConfigurationProperties configProps = config.getConfigurationProperties();
+        // Print all available configuration options
+        printConfigProperties(configProps);
+        // Here we could harcode the connect properties
+        // configProps.setPropertyValue("host", "myHost");
+        // But we rather use TestHelpers property bag
+        setConnectProperties(configProps);
+        ConnectorFacade facade = ConnectorFacadeFactory.getInstance().newInstance(config);
+        printSupportedOperations(facade);
+
+        facade.validate();
+        facade.test();
+
+        // Now use connector
+        findAllUsers(facade);
+        findSpecificUsers(facade);
+        createUser(facade);
+        updateUser(facade);
+        authenticateUser(facade);
     }
     
+    private static void logUsecaseStart(String name){
+        System.out.println("------------------------" + name + "-----------------------------------------------");
+    }
+
+    private static void updateUser(ConnectorFacade facade) {
+        logUsecaseStart("Update user");
+        Uid uid = facade.create(ObjectClass.ACCOUNT, CollectionUtil.newSet(new Name("testUser"), AttributeBuilder
+                .buildPassword("testPasswd".toCharArray()), AttributeBuilder.build("oraclePrivs", "CREATE SESSION")),
+                null);
+        // Update password , expire password and lock user
+        facade.update(ObjectClass.ACCOUNT, uid, CollectionUtil.newSet(AttributeBuilder.buildPassword("newPassword"
+                .toCharArray()), AttributeBuilder.buildEnabled(false), AttributeBuilder.buildPasswordExpired(true)),
+                null);
+        OperationOptions options = new OperationOptionsBuilder().setAttributesToGet("oracleRoles", "oraclePrivs",
+                OperationalAttributes.PASSWORD_EXPIRED_NAME, OperationalAttributes.ENABLE_NAME).build();
+        //Show user
+        ConnectorObject obj = facade.getObject(ObjectClass.ACCOUNT, uid, options);
+        System.out.println(obj);
+        System.out.println(obj.getAttributeByName("oraclePrivs"));
+        //Now add values to privileges
+        facade.addAttributeValues(ObjectClass.ACCOUNT, uid, CollectionUtil.newSet(AttributeBuilder.build("oraclePrivs",
+                "ALTER SESSION", "SELECT ANY TABLE","INSERT ANY TABLE")), null);
+        //Show privileges
+        obj = facade.getObject(ObjectClass.ACCOUNT, uid, options);
+        System.out.println(obj.getAttributeByName("oraclePrivs"));
+        //Remove one privilege
+        facade.removeAttributeValues(ObjectClass.ACCOUNT, uid, CollectionUtil.newSet(AttributeBuilder.build("oraclePrivs",
+                "SELECT ANY TABLE")), null);
+        //And again show privileges
+        obj = facade.getObject(ObjectClass.ACCOUNT, uid, options);
+        System.out.println(obj.getAttributeByName("oraclePrivs"));
+        facade.delete(ObjectClass.ACCOUNT, uid, null);
+        System.out.println();
+    }
+
     private static void findSpecificUsers(ConnectorFacade facade) {
-        System.out.println("Find specific users");
+        logUsecaseStart("Find specific users");
         ResultsHandler printHandler = new ResultsHandler() {
             public boolean handle(ConnectorObject obj) {
                 System.out.print(obj.getUid().getUidValue() + " ");
@@ -104,11 +141,8 @@ public class OracleConnectorTestApplication {
         System.out.println("Users starting on S");
         facade.search(ObjectClass.ACCOUNT, FilterBuilder.startsWith(new Name("S")), printHandler, null);
         System.out.println("Users starting on S and ending on R");
-        facade.search(ObjectClass.ACCOUNT,
-                FilterBuilder.and(
-                        FilterBuilder.startsWith(new Name("S")),
-                        FilterBuilder.endsWith(new Name("R"))),
-                printHandler, null);
+        facade.search(ObjectClass.ACCOUNT, FilterBuilder.and(FilterBuilder.startsWith(new Name("S")), FilterBuilder
+                .endsWith(new Name("R"))), printHandler, null);
         System.out.println();
     }
 
@@ -116,7 +150,7 @@ public class OracleConnectorTestApplication {
      * @param configProps
      */
     private static void printConfigProperties(ConfigurationProperties configProps) {
-        System.out.println("Config properties ");
+        logUsecaseStart("Config properties ");
         System.out.println(configProps.getPropertyNames());
         System.out.println();
     }
@@ -125,47 +159,44 @@ public class OracleConnectorTestApplication {
      * @param facade
      */
     private static void authenticateUser(ConnectorFacade facade) {
-        System.out.println("Authenticate user");
-        Uid uid = facade.create(ObjectClass.ACCOUNT, CollectionUtil.newSet(
-                new Name("testUser"),
-                AttributeBuilder.buildPassword("testPasswd".toCharArray()),
-                AttributeBuilder.build("oraclePrivs", "CREATE SESSION")),
+        logUsecaseStart("Authenticate user");
+        Uid uid = facade.create(ObjectClass.ACCOUNT, CollectionUtil.newSet(new Name("testUser"), AttributeBuilder
+                .buildPassword("testPasswd".toCharArray()), AttributeBuilder.build("oraclePrivs", "CREATE SESSION")),
                 null);
         facade.authenticate(ObjectClass.ACCOUNT, "TESTUSER", new GuardedString("testPasswd".toCharArray()), null);
-        facade.update(ObjectClass.ACCOUNT, uid, CollectionUtil.newSet(AttributeBuilder.buildPasswordExpired(true)), null);
-        try{
+        facade.update(ObjectClass.ACCOUNT, uid, CollectionUtil.newSet(AttributeBuilder.buildPasswordExpired(true)),
+                null);
+        try {
             facade.authenticate(ObjectClass.ACCOUNT, "TESTUSER", new GuardedString("testPasswd".toCharArray()), null);
-        }
-        catch(PasswordExpiredException e){
+        } catch (PasswordExpiredException e) {
             System.out.println(e.getMessage());
         }
-        
         facade.delete(ObjectClass.ACCOUNT, uid, null);
     }
 
     private static void createUser(ConnectorFacade facade) {
-       System.out.println("Creating user");
-       Uid uid = facade.create(ObjectClass.ACCOUNT, CollectionUtil.newSet(new Name("testUser"), AttributeBuilder.buildPassword("testPasswd".toCharArray())), null);
-       System.out.println("User created : " + uid.getUidValue());
-       ConnectorObject object = facade.getObject(ObjectClass.ACCOUNT, uid, null);
-       System.out.println("User attributes");
-       System.out.println(object);
-       try{
-           facade.create(ObjectClass.ACCOUNT, CollectionUtil.newSet(new Name("testUser"), AttributeBuilder.buildPassword("testPasswd".toCharArray())), null);
-       }
-       catch(AlreadyExistsException e){
-           System.out.println(e.getMessage());
-       }
-       facade.delete(ObjectClass.ACCOUNT, uid, null);
-       System.out.println();
+        logUsecaseStart("Creating user");
+        Uid uid = facade.create(ObjectClass.ACCOUNT, CollectionUtil.newSet(new Name("testUser"), AttributeBuilder
+                .buildPassword("testPasswd".toCharArray())), null);
+        System.out.println("User created : " + uid.getUidValue());
+        ConnectorObject object = facade.getObject(ObjectClass.ACCOUNT, uid, null);
+        System.out.println("User attributes");
+        System.out.println(object);
+        try {
+            facade.create(ObjectClass.ACCOUNT, CollectionUtil.newSet(new Name("testUser"), AttributeBuilder
+                    .buildPassword("testPasswd".toCharArray())), null);
+        } catch (AlreadyExistsException e) {
+            System.out.println(e.getMessage());
+        }
+        facade.delete(ObjectClass.ACCOUNT, uid, null);
+        System.out.println();
     }
 
     /**
      * @param facade
      */
     private static void findAllUsers(ConnectorFacade facade) {
-        System.out.println();
-        System.out.println("Find all users");
+        logUsecaseStart("Find all users");
         final int[] count = new int[1];
         ResultsHandler handler = new ResultsHandler() {
             public boolean handle(ConnectorObject obj) {
@@ -183,11 +214,10 @@ public class OracleConnectorTestApplication {
      * @param facade
      */
     private static void printSupportedOperations(ConnectorFacade facade) {
-        System.out.println("Supported operations");
+        logUsecaseStart("Supported operations");
         System.out.println(facade.getSupportedOperations());
         System.out.println();
 
-        
     }
 
     /**
@@ -200,9 +230,9 @@ public class OracleConnectorTestApplication {
         GuardedString password = new GuardedString(passwordString.toCharArray());
         String driver = OracleSpecifics.THIN_DRIVER;
         String host = testProps.getStringProperty("thin.host");
-        String port = testProps.getProperty("thin.port",String.class,"1524");
+        String port = testProps.getProperty("thin.port", String.class, "1524");
         String database = testProps.getStringProperty("thin.database");
-        
+
         configProps.setPropertyValue("host", host);
         configProps.setPropertyValue("port", port);
         configProps.setPropertyValue("driver", driver);
@@ -212,8 +242,8 @@ public class OracleConnectorTestApplication {
     }
 
     private static void printAvailableConnectors(List<ConnectorInfo> connectorInfos) {
-        System.out.println("Available connectors : ");
-        for(ConnectorInfo info : connectorInfos){
+        logUsecaseStart("Available connectors : ");
+        for (ConnectorInfo info : connectorInfos) {
             StringBuilder buffer = new StringBuilder();
             buffer.append("Display name : ").append(info.getConnectorDisplayName());
             buffer.append("|Key : ").append(info.getConnectorKey());
@@ -221,21 +251,21 @@ public class OracleConnectorTestApplication {
         }
         System.out.println();
     }
-    
-    private static ConnectorKey findOracleConnectorKey(List<ConnectorInfo> connectorInfos){
-        for(ConnectorInfo info : connectorInfos){
-            if("org.identityconnectors.oracle.OracleConnector".equals(info.getConnectorKey().getConnectorName())){
+
+    private static ConnectorKey findOracleConnectorKey(List<ConnectorInfo> connectorInfos) {
+        for (ConnectorInfo info : connectorInfos) {
+            if ("org.identityconnectors.oracle.OracleConnector".equals(info.getConnectorKey().getConnectorName())) {
                 return info.getConnectorKey();
             }
         }
         throw new IllegalStateException("No oracle connector available");
     }
 
-    private static URL[] findConnectorsJar(String[] args) throws MalformedURLException{
+    private static URL[] findConnectorsJar(String[] args) throws MalformedURLException {
         List<URL> urls = new ArrayList<URL>();
-        for(String jar : args){
+        for (String jar : args) {
             File file = new File(jar);
-            if(!file.exists()){
+            if (!file.exists()) {
                 throw new RuntimeException("Jar does not exist");
             }
             URL url = file.toURI().toURL();
@@ -243,6 +273,5 @@ public class OracleConnectorTestApplication {
         }
         return urls.toArray(new URL[urls.size()]);
     }
-    
-    
+
 }
