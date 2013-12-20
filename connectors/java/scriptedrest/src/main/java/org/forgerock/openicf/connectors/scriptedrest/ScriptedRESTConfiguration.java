46850d96a998a6c9186f1a25e801a44a4e01f551
Gael Allioux <gael.allioux@forgerock.com>
2013-12-20 19:14:43 +0000

    CR-2752 OPENICF-158 - ScriptedREST connector
    git-svn-id: https://svn.forgerock.org/openicf/trunk@6464 05b3e5af-d696-470f-a577-fd7599f74d3c

---
+++ connectors/java/scriptedrest/src/main/java/org/forgerock/openicf/connectors/scriptedrest/ScriptedRESTConfiguration.java
@@ -0,0 +1,172 @@
+/*
+ * DO NOT REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2013 ForgeRock AS All rights reserved.
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package org.forgerock.openicf.connectors.scriptedrest;
+
+import org.forgerock.openicf.misc.scriptedcommon.ScriptedConfiguration;
+import org.identityconnectors.common.StringUtil;
+import org.identityconnectors.common.security.GuardedString;
+import org.identityconnectors.framework.spi.AbstractConfiguration;
+import org.identityconnectors.framework.spi.ConfigurationProperty;
+
+/**
+ * Extends the {@link AbstractConfiguration} class to provide all the necessary
+ * parameters to initialize the ScriptedREST Connector.
+ *
+ * @author Gael Allioux <gael.allioux@gmail.com>
+ * @version 1.1.0.0
+ */
+public class ScriptedRESTConfiguration extends ScriptedConfiguration {
+
+    // Exposed configuration properties.
+    /**
+     * The Remote user to authenticate with.
+     */
+    private String login = null;
+    /**
+     * The Password to authenticate with.
+     */
+    private GuardedString password = null;
+
+    /**
+     * Constructor.
+     */
+    public ScriptedRESTConfiguration() {
+    }
+
+    @ConfigurationProperty(order = 1, displayMessageKey = "remoteUser.display",
+            groupMessageKey = "basic.group", helpMessageKey = "remoteUser.help",
+            required = true, confidential = false)
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @ConfigurationProperty(order = 2, displayMessageKey = "password.display",
+            groupMessageKey = "basic.group", helpMessageKey = "password.help",
+            confidential = true)
+    public GuardedString getPassword() {
+        return password;
+    }
+
+    public void setPassword(GuardedString password) {
+        this.password = password;
+    }
+    // ===============================================
+    // HTTP connection
+    // ===============================================
+    /*
+     * Endpoint
+     */
+    private String endPoint = "http://localhost:8080/openidm";
+
+    /**
+     * Return the REST endpoint
+     *
+     * @return endpoint value
+     */
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    /**
+     * Set the REST endpoint
+     *
+     * @param value
+     */
+    public void setEndPoint(String endPoint) {
+        this.endPoint = endPoint;
+    }
+    /*
+     * Base path
+     */
+    private String basePath = "/openidm";
+
+    /**
+     * Return the REST base path
+     *
+     * @return endpoint value
+     */
+    public String getBasePath() {
+        return basePath;
+    }
+
+    /**
+     * Set the REST base path
+     *
+     * @param basePAth
+     */
+    public void setBasePath(String basePath) {
+        this.basePath = basePath;
+    }
+    /*
+     * default content type
+     * Can be: 
+     *  ANY
+     *  TEXT("text/plain")
+     *  JSON("application/json","application/javascript","text/javascript")
+     *  XML("application/xml","text/xml","application/xhtml+xml","application/atom+xml")
+     *  HTML("text/html")
+     *  URLENC("application/x-www-form-urlencoded")
+     *  BINARY("application/octet-stream")
+     */
+    private String defaultContentType = "JSON";
+
+    public void setDefaultContentType(String defaultContentType) {
+        this.defaultContentType = defaultContentType;
+    }
+
+    public String getDefaultContentType() {
+        return defaultContentType;
+    }
+    /*
+     * authMethod
+     * Can be:
+     *  BASIC
+     *  BASIC_PREEMPTIVE
+     *  CERT
+     *  OAUTH
+     */
+    private String defaultAuthMethod = "BASIC";
+
+    public String getDefaultAuthMethod() {
+        return defaultAuthMethod;
+    }
+
+    public void setDefaultAuthMethod(String defaultAuthMethod) {
+        this.defaultAuthMethod = defaultAuthMethod;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void validate() {
+        if (StringUtil.isBlank(login)) {
+            throw new IllegalArgumentException("Remote User cannot be null or empty.");
+        }
+    }
+}
---
+++ connectors/java/scriptedrest/src/main/resources/samples/OpenDJ/UpdateScript.groovy
@@ -0,0 +1,102 @@
+/*
+ *
+ * Copyright (c) 2013 ForgeRock Inc. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1.php or
+ * OpenIDM/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at OpenIDM/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted 2010 [name of copyright owner]"
+ *
+ * @author Gael Allioux <gael.allioux@forgerock.com>
+ */
+
+// Parameters:
+// The connector sends us the following:
+// connection : handler to the REST Client 
+// (see: http://groovy.codehaus.org/modules/http-builder/apidocs/groovyx/net/http/RESTClient.html)
+//
+// action: String correponding to the action (UPDATE/ADD_ATTRIBUTE_VALUES/REMOVE_ATTRIBUTE_VALUES)
+//   - UPDATE : For each input attribute, replace all of the current values of that attribute
+//     in the target object with the values of that attribute.
+//   - ADD_ATTRIBUTE_VALUES: For each attribute that the input set contains, add to the current values
+//     of that attribute in the target object all of the values of that attribute in the input set.
+//   - REMOVE_ATTRIBUTE_VALUES: For each attribute that the input set contains, remove from the current values
+//     of that attribute in the target object any value that matches one of the values of the attribute from the input set.
+
+// log: a handler to the Log facility
+// objectClass: a String describing the Object class (__ACCOUNT__ / __GROUP__ / other)
+// uid: a String representing the entry uid
+// attributes: an Attribute Map, containg the <String> attribute name as a key
+// and the <List> attribute value(s) as value.
+// password: password string, clear text (only for UPDATE)
+// options: a handler to the OperationOptions Map
+
+log.info("Entering "+action+" Script");
+
+// we assume UPDATE action
+
+// We can use Groovy template engine to generate the JSON body
+def engine = new groovy.text.SimpleTemplateEngine();
+
+def userTemplate = '''
+{
+  "_id": "$id",
+  "contactInformation": {
+    "telephoneNumber": "$telephoneNumber",
+    "emailAddress": "$emailAddress"
+  },
+  "name": {
+    "familyName": "$familyName",
+    "givenName": "$givenName"
+  },
+  "displayName": "$displayName"
+ }
+ '''
+
+def groupTemplate = '''
+{
+    "_id": "$id",
+    "members": [
+    <% members.each { %>{ "_id": "$it"},<% } %>
+    {"_id": "$last"}
+    ]
+}
+'''
+
+switch ( objectClass ) {
+case "__ACCOUNT__":
+    def binding = [
+        id: uid,
+        familyName: (attributes.get("familyName") == null)? "": attributes.get("familyName").get(0),
+        givenName: (attributes.get("givenName") == null)? "": attributes.get("givenName").get(0),
+        emailAddress: (attributes.get("emailAddress") == null)? "": attributes.get("emailAddress").get(0),
+        telephoneNumber: (attributes.get("telephoneNumber") == null)? "": attributes.get("telephoneNumber").get(0),
+        displayName: (attributes.get("displayName") == null)? "": attributes.get("displayName").get(0),
+    ];
+    connection.put( path : '/users/'+uid, body: engine.createTemplate(userTemplate).make(binding).toString());
+    break
+
+case "__GROUP__":
+    if (attributes.get("members") != null){
+        members = []
+        members.addAll(attributes.get("members"));
+        last = members.pop();
+        def binding = [id: uid,members: members,last: last];
+        connection.put( path : '/groups/'+uid, body: engine.createTemplate(groupTemplate).make(binding).toString());
+    }
+}
+return uid;
